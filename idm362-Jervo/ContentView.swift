56560034c591ecf32cd1796764d682cf313c7aa1
Jervis Thompson <jervo@mac.com>
2025-01-14 18:57:56 -0500

    Initial Commit

---
+++ idm362-Jervo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  idm362-Jervo
+//
+//  Created by Jervis Thompson on 1/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ idm362-Jervo/idm362_JervoApp.swift
@@ -0,0 +1,17 @@
+//
+//  idm362_JervoApp.swift
+//  idm362-Jervo
+//
+//  Created by Jervis Thompson on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct idm362_JervoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
